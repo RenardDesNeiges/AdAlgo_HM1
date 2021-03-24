c8bbb812fb454e3216f6b650a918b1578030d702
Titouan Renard <titouan.renard@gmail.com>
2021-03-24 20:42:31 +0100

    turbo_algo_deniz

--- implementation_exercise/turbo_algo_deniz.py
+++ implementation_exercise/turbo_algo_deniz.py
@@ -16,33 +16,25 @@ def addToDeltas(delta, id):
 def turbo_greddy(deltas,edges,n):
     cost = 0
     while deltas[0][0] > 1:
-        # print(deltas)
         (v, vid) = heapq._heappop_max(deltas)
         cost = cost + 2
-        # print("v : " + str(v) + " ; id : " + str(id))
         t = 0
         if(vid > n):
             t  = 1
-            
-        # print("VID = " + str(vid))
+        
+
         for e in edges:
             if(e[t] == vid):
 
-                did = e[1]
-                if t==1 :
-                    did = e[0]
-                # print(e)
-                # print("DID = " + str(did))
+                did = e[1-t]
+                
                 for i in range(len(deltas)):
-                    # print(deltas[i])
-                    # print(deltas[i])
                     if(deltas[i][1] == did):
-                        # print("decreasing "+str(i))
                         deltas[i][0] = deltas[i][0] - 1
-                        heapq._siftdown_max(deltas,i,i)
-        # heapq._heapify_max(deltas)
+                        # heapq._siftdown_max(deltas,i,i)
+        
+        heapq._heapify_max(deltas)
 
-    # print(deltas)
     cost = cost + sum(d for d, _ in deltas)/2
     return int(cost)
 
@@ -71,4 +63,7 @@ for line in sys.stdin:
 #for good performance we need delta to be a maxheap so let's heapify it
 heapq._heapify_max(deltas)
 
-print(turbo_greddy(deltas,edges,n))
+if m == 0:
+    print(0)
+else:
+    print(turbo_greddy(deltas,edges,n))
